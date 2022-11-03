c09743f4a13d4f5f8de948373ab8dd8ce17b8aef
Sébastien Briois <sebastien.briois@evotec.com>
2022-11-03 11:46:53 +0100

    initial commit

---
+++ question_1.py
@@ -0,0 +1,4 @@
+# create a function that returns an array of 2 dimensions:
+# each cell value will be the multiplication of its x, y position (indices) in the array, base 1.
+# for example: cell value at position 2, 3 will be 2 x 3 = 6
+# The function takes 2 parameters: number of rows, number of columns to define the array dimensions
