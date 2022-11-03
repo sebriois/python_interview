74bb21c29bedc92310e143baf7a854074dddc515
Sébastien Briois <sebastien.briois@evotec.com>
2022-11-03 11:56:12 +0100

    adding question 2

---
+++ question_2.py
@@ -0,0 +1,6 @@
+# given the attached fasta file, return the DNA sequence name that has the highest GC-content
+# The GC-content of a DNA string is given by the percentage of symbols in the string that are 'C' or 'G'.
+#
+# Expected output:
+# sequence_0808
+# 60.919540
