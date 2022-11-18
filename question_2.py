c5067e92191361b77da410852ec91e559bba0b79
Sami <sami.zaizafoun@gmail.com>
2022-11-18 13:47:49 +0100

    Questions 1 and 2 solved

--- question_2.py
+++ question_2.py
@@ -4,3 +4,34 @@
 # Expected output:
 # sequence_0808
 # 60.919540
+
+def read_fasta(file_name):
+    fasta_file = open(file_name)
+    name = ''
+    sequences = {}
+
+    for line in fasta_file:
+        if line.startswith('>'):
+            name = line[1:].rstrip('\n')
+            sequences[name] = ''
+        else:
+            sequences[name] += line.rstrip('\n').rstrip('*')
+    return sequences
+
+
+def count_symbols(sequences):
+    gc_content = {}
+    counter = 0
+    for i in sequences:
+        gc_content[i] = ''
+        for symbol in sequences[i]:
+            if symbol == "C" or symbol == "G":
+                counter += 1
+        gc_content[i] = (counter/len(sequences[i]))*100
+        counter = 0
+    max_percentage = max(gc_content, key=lambda x: gc_content[x])
+    return max_percentage + "\n" + str(gc_content[max_percentage])
+
+
+sequence_list = read_fasta("question_2.fasta")
+print(count_symbols(sequence_list))
